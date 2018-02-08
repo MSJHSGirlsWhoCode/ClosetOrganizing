df6508709bcec8c01eefa4e628c09d0beab31d9a
katester35 <katester35@gmail.com>
2018-02-07 16:30:24 -0800

    New index

---
+++ index1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- your webpage info goes here -->
+
+    <title>Closet Organizing</title>
+
+	<meta name="author" content="your name" />
+	<meta name="description" content="" />
+
+<!-- you should always add your stylesheet (css) in the head tag so that it starts loading before the page html is being displayed -->
+	<link rel="stylesheet" href="style.css" type="text/css" />
+
+</head>
+<body>
+
+<!-- webpage content goes here in the body -->
+
+	<div id="page">
+		<div id="logo"><h1>Closet Organizer</h1>
+		</div>
+		<div id="nav">
+			<ul>
+				<li><a href="closetOrganizer.html">Home</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</div>
+		<div id="content">
+
+			<p>
+        <button type="button" class="button"><a href="form.php">What should you wear? (Click here!)</a></button>
+        <img id="home" src="colorfulcloset.jpg" alt="Empty Closet">
+			</p>
+		</div>
+		<div id="footer">
+			<p>
+				Webpage made by <a href="/" target="_blank">Anya, Katy, and Sanya</a>
+			</p>
+		</div>
+	</div>
+</body>
+</html>
+Add Comment Collapse
